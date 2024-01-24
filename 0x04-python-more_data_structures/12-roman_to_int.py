9ede00d3caa614d7bab2d5e61a0b3c8318d6de07
AyoDavid26 <folayanayodeji@gmail.com>
2024-01-24 15:59:06 -0800

    MoRe DatA StrUcTUres

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,17 +3,17 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
         return 0
-    roman_numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     result = 0
     previous_value = 0
-    
+
     for char in reversed(roman_string):
-        value = roman_numbers[char]
+        value = rom_num[char]
         if value >= previous_value:
             result += value
         else:
             result -= value
-        previouse_value = value
+        previous_value = value
 
     return result
