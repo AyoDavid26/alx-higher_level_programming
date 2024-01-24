c4a1db4c4cf483920e7104a60434354c5fe98871
AyoDavid26 <folayanayodeji@gmail.com>
2024-01-24 14:18:30 -0800

    MoRe DatA StrUcTUres

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    my_matrix = [new[:] for new in matrix]
-    for idx, new in enumerate(my_matrix):
-        for idx2, col in enumerate(my_matrix):
-            my_matrix[idx][idx2] = new[idx2] ** 2
-    return my_matrix
+    new_matrix = [row[:] for row in matrix]
+    for idx, row in enumerate(new_matrix):
+        for idx2, col in enumerate(new_matrix):
+            new_matrix[idx][idx2] = new[idx2] ** 2
+    return new_matrix
