403f4426365790b6208f164b80695403bce74e69
AyoDavid26 <folayanayodeji@gmail.com>
2024-01-24 16:34:53 -0800

    MoRe DatA StrUcTUres

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    [a_dictionary.pop(key) for key, v in list(a_dictionary.items()) if v == value]
+    [a_dictionary.pop(k) for k, v in list(a_dictionary.items()) if v == value]
     return a_dictionary
