0ef1e39fec7110b30a1a5f1f2a57299ac99b4d29
AyoDavid26 <folayanayodeji@gmail.com>
2024-03-27 17:57:24 -0700

    More Queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,8 @@
 -- To create database hbtn_0d_2 and user user_0d_2
 -- user_0d_2 has SELECT privileges on htbn_0d_2with password
 CREATE DATABASE
-    IF NOT EXISTS `htbn_0d_2;
-CRREATE USER
+    IF NOT EXISTS `htbn_0d_2`;
+CREATE USER
     IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
