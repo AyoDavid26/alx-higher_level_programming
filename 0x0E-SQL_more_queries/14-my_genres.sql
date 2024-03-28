6a16f4df9f6bb5a16c52c1040f97a1b5c510e222
AyoDavid26 <folayanayodeji@gmail.com>
2024-03-27 19:43:49 -0700

    More Queries

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,7 +2,7 @@
 -- Results are sorted in ascending order
 SELECT g.`name`
   FROM `tv_genres` AS g
-	INNER JOIN `tv_show_genre` AS s
+	INNER JOIN `tv_show_genres` AS s
 	ON g.`id` = s.`genre_id`
 
 	INNER JOIN `tv_shows` AS t
