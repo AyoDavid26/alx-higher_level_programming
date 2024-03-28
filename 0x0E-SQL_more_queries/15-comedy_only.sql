8cc411973c0381557f977f6a0694883db1f422c5
AyoDavid26 <folayanayodeji@gmail.com>
2024-03-27 19:32:53 -0700

    More Queries

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -5,7 +5,7 @@ SELECT t.`title`
        INNER JOIN `tv_shows_genres` AS s
        ON t.`id` = s.`show_id`
 
-       INNER JOIN `tv_genres` AS p
-       ON p.`id` = s.`genre_id`
-       WHERE P.`name` = "Comedy"
+       INNER JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE g.`name` = "Comedy"
 ORDER BY t.`title`;
