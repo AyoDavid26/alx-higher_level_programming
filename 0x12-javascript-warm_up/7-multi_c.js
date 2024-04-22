f13e317445f89afb8e88b97b792d4ae09bedf02f
AyoDavid26 <folayanayodeji@gmail.com>
2024-04-22 16:09:32 -0700

    Javascript Warmup

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const s =math.floor(Number(process.argv[2]));
+if (isNaN(s) {
+  console.log('Missing number of occurences');
+} else {
+   for (let i = 0; i < s; i++) {
+     console.log('C is fun');
+  }
+}
