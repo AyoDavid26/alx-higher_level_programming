f6439d59f357a957e9de4eb38f1b8d76e96d15a5
AyoDavid26 <folayanayodeji@gmail.com>
2024-05-09 21:01:43 -0700

    Python_Networks

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,24 +3,21 @@
 
 
 def find_peak(list_of_integers):
-    """ finding the peak number with the shortest algorithm"""
+    """Return a peak in a list of unsorted integers."""
     if list_of_integers == []:
         return None
 
     size = len(list_of_integers)
-    if size == 0:
-        return (None)
-    elif size == 1:
-        return (list_of_integers[0])
-    elif size ==2:
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
         return max(list_of_integers)
 
-    mid = int(size/2)
+    mid = int(size / 2)
     peak = list_of_integers[mid]
-    mylist = list_of_integers
-    if peak > mylist[mid - 1] and peak > mylist[mid + 1]:
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
-    elif peak < mylist[mid - 1]:
-        return find_peak(mylist[:mid])
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
     else:
-        return find_peak(mylist + 1:])
+        return find_peak(list_of_integers[mid + 1:])
